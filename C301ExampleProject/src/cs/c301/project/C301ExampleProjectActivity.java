017f69f5661f7c8a61ac4cd56f2b90d3386b98ba
esteckle <esteckle@ualberta.ca>
2012-02-07 17:31:09 -0700

    example 1

---
+++ C301ExampleProject/src/cs/c301/project/C301ExampleProjectActivity.java
@@ -0,0 +1,13 @@
+package cs.c301.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class C301ExampleProjectActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
